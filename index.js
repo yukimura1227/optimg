a93892327ebdff940dfa4aa4c45074e74706530c
yukimura1227 <takamura1227@gmail.com>
2018-07-27 10:03:46 +0900

    implements optimize function

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 "use strict";
 const electron = require("electron");
-const {app, BrowserWindow, shell} = electron;
+const {app, BrowserWindow, ipcMain, shell} = electron;
+const fs = require("fs");
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
 
 global.mainWindow = null;
 
 app.on('ready', () => {
+  setup_env();
   createWindow();
 });
 
@@ -14,6 +18,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
+  setup_env();
   createWindow();
 });
 
@@ -32,3 +37,35 @@ function createWindow() {
   global.mainWindow.on('closed', () => global.mainWindow = null );
 }
 
+ipcMain.on('ipc_optimize_image', (event, arg) => {
+  optimize_image();
+});
+
+ipcMain.on('ipc_open_input_dir', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/input/');
+});
+
+ipcMain.on('ipc_open_output_dir', (event, arg) => {
+  shell.showItemInFolder(app.getPath('userData') + '/output/');
+});
+
+function optimize_image() {
+  const input_dir = app.getPath('userData') + '/input/';
+  const output_dir = app.getPath('userData') + '/output/';
+  imagemin([input_dir+ '*.png'], output_dir, {
+    plugins: [
+      imageminPngquant()
+    ]
+  });
+}
+
+function setup_env() {
+  const input_dir = app.getPath('userData') + '/input/';
+  const output_dir = app.getPath('userData') + '/output/';
+  if(!fs.existsSync(input_dir)) {
+    fs.mkdirSync(input_dir);
+  }
+  if(!fs.existsSync(output_dir)) {
+    fs.mkdirSync(output_dir);
+  }
+}
